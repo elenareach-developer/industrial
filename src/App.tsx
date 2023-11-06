ad52e322ebd3dfb6f96ea74646498284b56129b0
elenareach@gmail.com <elenareach@gmail.com>
2023-11-05 17:49:48 -0700

    update App.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { ExternalLinkIcon, MenuIcon} from '@heroicons/react/outline'
 import React from 'react'
 import Hero from './components/Hero'
 import Footer from './components/Footer'
 
 const App = () => {
-  const currentYear: number = new Date().getFullYear()
+
 
   return (
     <>
@@ -12,14 +11,14 @@ const App = () => {
       <div className="min-h-screen flex flex-col w-full dark:bg-gray-900 dark:text-white">
         <div className="w-full p-4 text-center bg-gray-800 text-gray-400">
           <div className="container mx-auto">
-          <div>React Tutorals</div>
+          <div>React Tutorial</div>
           </div>
         </div>
         <div className="w-full p-4 text-center text-gray-400">
         <Hero />
         </div>
-        <Footer/>
-      </div>
+        <Footer />
+     </div>
 
     </>
   )
